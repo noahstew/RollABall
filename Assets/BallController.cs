3d7d2397293f267bbde4d96c977144daf8bcd1da
Noah Stewart <njstewart22@icloud.com>
2025-01-14 08:56:25 -0800

    Using SRP for Ball Control

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,35 +1,12 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BallController : MonoBehaviour
 {
-    public Rigidbody sphereRigidbody;
-    public float ballSpeed = 2f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
+    [SerializeField] private Rigidbody sphereRigidbody;
+    [SerializeField] private float ballSpeed = 2f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 inputVector = Vector2.zero;
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector += Vector2.up;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector += Vector2.right;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector += Vector2.left;
-        }
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
+    public void moveBall(Vector2 input) { 
+        Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 }
