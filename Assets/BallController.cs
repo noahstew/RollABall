293ea14ee9c530d8c6cc97647dbfbb8a0f49cf90
Noah Stewart <njstewart22@icloud.com>
2025-01-14 09:06:20 -0800

    Adding Walls

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] private float ballSpeed = 2f;
+    [SerializeField] private float ballSpeed = 4f;
 
     public void moveBall(Vector2 input) { 
         Vector3 inputXZPlane = new Vector3(input.x, 0, input.y);
