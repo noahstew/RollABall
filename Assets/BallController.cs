df28fa0b4265cf6ff3bacedc55d37d2ce75e328c
Noah Stewart <njstewart22@icloud.com>
2025-01-14 16:55:54 -0800

    Implementing Jump Function to Player

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,14 +2,38 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] private float ballSpeed = 4f;
-    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private Rigidbody sphereRigidbody; // Ball Rigidbody
+    [SerializeField] private float ballSpeed = 4f; // Movement speed
+    [SerializeField] private float jumpForce = 10f; // Jump force
+    private bool isGrounded = false; // Grounded state
 
-    public void moveBall(Vector3 input) { 
-        Vector3 inputXZPlane = new Vector3(input.x, 0, input.z);
-        Vector3 inputYPlane = new Vector3(0, input.y, 0);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
-        sphereRigidbody.AddForce(inputYPlane * jumpForce);
+    // Detect collision when landing
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 3) // Detects if ground (layer 3) is touched
+        {
+            isGrounded = true;
+        }
+    }
+
+    // Detect collision when leaping
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 3) // Detects if ground (layer 3) is touched
+        {
+            isGrounded = false;
+        }
+    }
+
+    // Move the ball
+    public void moveBall(Vector3 input)
+    {
+        Vector3 inputXZPlane = new Vector3(input.x, 0, input.z); // Vector 3 representation of input
+        sphereRigidbody.AddForce(inputXZPlane * ballSpeed); // Moving ball
+
+        if (input.y > 0 && isGrounded) // Jump check
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Instant velocity change in Y direction
+        }
     }
 }
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -3,33 +3,45 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-
-    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>();
+    // Changed to Vector 3 to support jump 
+    public UnityEvent<Vector3> OnMove = new UnityEvent<Vector3>(); 
 
     void Update()
     {
+        // Zeroing input
         Vector3 inputVector = Vector3.zero;
-        if (Input.GetKey(KeyCode.W))
+
+        // Forward
+        if (Input.GetKey(KeyCode.W)) 
         {
-            inputVector += Vector3.forward;
+            inputVector += Vector3.forward; 
         }
+
+        // Backward
         if (Input.GetKey(KeyCode.S))
         {
             inputVector += Vector3.back;
         }
+
+        // Left
         if (Input.GetKey(KeyCode.A))
         {
             inputVector += Vector3.left;
         }
+
+        // Right
         if (Input.GetKey(KeyCode.D))
         {
             inputVector += Vector3.right;
         }
+
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
             inputVector += Vector3.up;
         }
 
+        // Invoking Ball Controller
         OnMove?.Invoke(inputVector);
     }
 }
